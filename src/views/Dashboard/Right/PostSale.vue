f7f8061f34a1738fa36270cb524fa6a363919b5e
xiyuu <1464263252@qq.com>
2024-08-05 18:05:10 +0800

    css update

--- src/views/Dashboard/Right/PostSale.vue
+++ src/views/Dashboard/Right/PostSale.vue
@@ -12,19 +12,19 @@
           <div class="num font-bold text-xl">{{ 0 }}</div>
         </div>
         <div
-          class="item p-4 bg-[--info-bg-color] cursor-pointer hover:bg-[--info-bg-hover-color] transition-all duration-300]"
+          class="item px-8 py-4 bg-[--info-bg-color] cursor-pointer hover:bg-[--info-bg-hover-color] transition-all duration-300]"
         >
           <div class="text mb-2 text-sm">仅退货</div>
           <div class="num font-bold text-xl">{{ 0 }}</div>
         </div>
         <div
-          class="item p-4 bg-[--info-bg-color] cursor-pointer hover:bg-[--info-bg-hover-color] transition-all duration-300]"
+          class="item px-8 py-4 bg-[--info-bg-color] cursor-pointer hover:bg-[--info-bg-hover-color] transition-all duration-300]"
         >
           <div class="text mb-2 text-sm">仅退款</div>
           <div class="num font-bold text-xl">{{ 0 }}</div>
         </div>
         <div
-          class="item p-4 bg-[--info-bg-color] cursor-pointer hover:bg-[--info-bg-hover-color] transition-all duration-300]"
+          class="item px-8 py-4 bg-[--info-bg-color] cursor-pointer hover:bg-[--info-bg-hover-color] transition-all duration-300]"
         >
           <div class="text mb-2 text-sm">退换货</div>
           <div class="num font-bold text-xl">{{ 0 }}</div>
@@ -33,6 +33,5 @@
     </div>
   </div>
 </template>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <style lang="scss" scoped></style>
